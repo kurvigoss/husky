9437e9bb2f02a7087dffc7852a6256be938cc201
yuantongqin <yuantongqin@odianyun.com>
2019-04-25 09:27:14 +0800

    测试接口

--- test-service/test-server/src/main/java/com/husky/test/spi/TestSPI.java
+++ test-service/test-server/src/main/java/com/husky/test/spi/TestSPI.java
@@ -1,11 +1,14 @@
 package com.husky.test.spi;
 
+import java.lang.reflect.Method;
+
 import com.husky.common.dto.HuskyResult;
 import com.husky.test.api.TestAPI;
 import com.husky.test.dto.TestDTO;
 import com.husky.user.api.UserAPI;
 import com.husky.user.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,5 +29,22 @@ public class TestSPI implements TestAPI {
         testDTO.setTestName("test001");
         testDTO.setUserJsonData(userDTOHuskyResult.getData().toString());
         return HuskyResult.doSuccess(testDTO);
+    }
+
+    @GetMapping("/hello.do")
+    public String hello(String name){
+        return "你好："+name;
+    }
+
+    public void ss(){
+        try {
+            Class<?> aClass = Class.forName("");
+            Method method = aClass.getMethod("", TestAPI.class);
+//            method.invoke()
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
