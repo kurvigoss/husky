8c37ffc7f5e2186584324529216455babc695c11
gexiaobing <gexiao_bing@163.com>
2019-04-25 15:49:48 +0800

    提交代码

--- service-base/src/main/java/com/husky/handler/HuskyControllerExceptionHandler.java
+++ service-base/src/main/java/com/husky/handler/HuskyControllerExceptionHandler.java
@@ -23,6 +23,7 @@ public class HuskyControllerExceptionHandler {
     @ResponseStatus(HttpStatus.OK)
     public HuskyResult resolveException(Exception e){
         logger.error(e.getMessage(),e);
+        System.out.println();
         if(e instanceof HuskyException){
             return HuskyResult.doError((HuskyException)e);
         }else {
